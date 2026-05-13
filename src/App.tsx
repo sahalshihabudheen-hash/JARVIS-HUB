1c1c544218bbcf08755b264687b69c588159a6db
Sahal Shihabudheen <sahalshihabudheen@gmail.com>
2026-05-13 11:07:17 +0530

    Debug: Temporarily disabled JarvisTutorial to investigate black screen

--- src/App.tsx
+++ src/App.tsx
@@ -178,7 +178,7 @@ const App = () => (
                 <StealthManager />
                 <RemoteListener />
                 <CommandPalette />
-                <JarvisTutorial />
+                {/* <JarvisTutorial /> */}
                 <JarvisOrb />
                 <VerificationBanner />
                 <Toaster />
